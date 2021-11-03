f9937e21b28ef280f128f5443f1e2ca4f147ed69
rodrigo-militao <militaodorigo9@gmail.com>
2021-11-03 14:16:05 -0300

    implementacao da entidade Transaction, e o ITransactionRepository

---
+++ MM4Bank.Domain/Entities/Transaction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MM4Bank.Domain.Validation;
+namespace MM4Bank.Domain.Entities
+{
+    public sealed class Transaction : Entity
+    {
+        public double Valor { get; set; }
+        public DateTime DataTransferencia { get; set; } = DateTime.Now;
+        public Account ContaOrigem { get; set; }
+        public Account ContaDestino { get; set; }
+
+        public Transaction(double valor, Account contaOrigem, Account contaDestino)
+        {
+            ValidateDomain(valor, contaOrigem, contaDestino);
+        }
+
+        private void ValidateDomain(double valor, Account contaOrigem, Account contaDestino)
+        {
+            DomainExceptionValidation.When(double.IsNaN(valor), "Invalid value.");
+            DomainExceptionValidation.When(valor <= 0, "Invalid value, value must be greater than 0");
+
+            Valor = valor;
+            ContaOrigem = contaOrigem;
+            ContaDestino = contaDestino;
+        }
+    }
+}
---
+++ MM4Bank.Domain/Interfaces/ITransactionRepository.cs
@@ -0,0 +1,19 @@
+﻿using MM4Bank.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MM4Bank.Domain.Interfaces
+{
+    public interface ITransactionRepository
+    {
+        Task<IEnumerable<Transaction>> GetTransactions();
+        Task<Transaction> GetById(int? id);
+        Task<Transaction> Create(Transaction transaction);
+        Task<Transaction> Update(Transaction transaction);
+        Task<Transaction> Remove(Transaction transaction);
+
+    }
+}
