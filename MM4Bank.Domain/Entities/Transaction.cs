bf67c8e9605baf63692e31e36163581d6e5c1097
Julio <juliokauer@gmail.com>
2021-11-04 15:06:08 -0300

    Refactorate Transaction

--- MM4Bank.Domain/Entities/Transaction.cs
+++ MM4Bank.Domain/Entities/Transaction.cs
@@ -2,25 +2,26 @@
 using MM4Bank.Domain.Validation;
 namespace MM4Bank.Domain.Entities
 {
-    public sealed class Transaction : Entity
+  public sealed class Transaction : Entity
     {
         public decimal Value { get; set; }
         public Account SourceAccount { get; set; }
         public Account TargetAccount { get; set; }
+        public TransactionType Type { get; set; }
 
-        public Transaction(decimal value, Account sourceAccount, Account targetAccount)
+        public Transaction(decimal value, Account sourceAccount, Account targetAccount, TransactionType type)
         {
-            ValidateDomain(value, targetAccount, sourceAccount);
-        }
-
-        private void ValidateDomain(decimal value, Account sourceAccount, Account targetAccount)
-        {
-            DomainExceptionValidation.When(value > sourceAccount.Balance, "Not enough balance on source account.");
-            DomainExceptionValidation.When(value <= 0, "Invalid value, value must be greater than 0");
-
+            ValidateDomain(value, sourceAccount);
             Value = value;
             SourceAccount = sourceAccount;
             TargetAccount = targetAccount;
+            Type = type;
+        }
+
+        private void ValidateDomain(decimal value, Account sourceAccount)
+        {
+            DomainExceptionValidation.When(value > sourceAccount.Balance, "Not enough balance on source account.");
+            DomainExceptionValidation.When(value <= 0, "Invalid value, value must be greater than 0");
         }
     }
 }
