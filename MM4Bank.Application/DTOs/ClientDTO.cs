f808464d715b7d71fa4bf7d9400569a3cb9bab02
sdayube <scarige@gmail.com>
2021-11-13 17:28:21 -0300

    Add required using statements to DTO files

--- MM4Bank.Application/DTOs/ClientDTO.cs
+++ MM4Bank.Application/DTOs/ClientDTO.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using MM4Bank.Domain.ValueObjects;
+using MM4Bank.Domain.Entities;
 
 namespace MM4Bank.Application.DTOs
 {
@@ -12,18 +14,18 @@ namespace MM4Bank.Application.DTOs
 
         public Guid Id { get; set; }
 
-        [Required(ErrorMessage="Name is required!")]
+        [Required(ErrorMessage = "Name is required!")]
         public Name Name { get; set; }
 
-        [Required(ErrorMessage="CPF is required!")]
+        [Required(ErrorMessage = "CPF is required!")]
         public CPF CPF { get; set; }
 
-        [Required(ErrorMessage="Account is required!")]
+        [Required(ErrorMessage = "Account is required!")]
         public Account Account { get; set; }
 
         public Guid AccountId { get; set; }
 
-        [Required(ErrorMessage="Address is required!")]
+        [Required(ErrorMessage = "Address is required!")]
         public Address Address { get; set; }
 
         public DateTime CreatedAt { get; set; }
