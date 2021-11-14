00e1e1174d851aed26ddac6c612dd84e8ce6a09b
sdayube <scarige@gmail.com>
2021-11-14 20:08:29 -0300

    Add transaction queries

---
+++ MM4Bank.Application/TransactionsCQRS/Queries/GetTransactionByIdQuery.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using MM4Bank.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MM4Bank.Application.Transactions.Queries
+{
+    public class GetTransactionByIdQuery : IRequest<Transaction>
+    {
+        public Guid Id { get; set; }
+
+        public GetTransactionByIdQuery(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
