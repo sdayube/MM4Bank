caf6338278a425f9208ec8f2df3824b5fdab609c
sdayube <scarige@gmail.com>
2021-11-14 13:59:39 -0300

    Implement Application layer

---
+++ MM4Bank.Application/Interfaces/IAccountService.cs
@@ -0,0 +1,18 @@
+﻿using MM4Bank.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MM4Bank.Application.Interfaces
+{
+    public interface IAccountService
+    {
+        Task<IEnumerable<AccountDTO>> GetAccountsAsync();
+        Task<AccountDTO> GetByIdAsync(Guid? id);
+        Task AddAsync(AccountDTO accountDTO);
+        Task UpdateAsync(AccountDTO accountDTO);
+        Task RemoveAsync(Guid? id);
+    }
+}
---
+++ MM4Bank.Application/Services/ClientService.cs
@@ -0,0 +1,74 @@
+﻿using AutoMapper;
+using MM4Bank.Application.DTOs;
+using MM4Bank.Application.Interfaces;
+using MM4Bank.Domain.Entities;
+using MM4Bank.Domain.Interfaces;
+using MM4Bank.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MM4Bank.Application.Services
+{
+    public class ClientService : IClientService
+    {
+        private readonly IClientRepository _clientRepository;
+        private readonly IMapper _mapper;
+
+        public ClientService(IClientRepository clientRepository, IMapper mapper)
+        {
+            _clientRepository = clientRepository ?? throw new ArgumentNullException(nameof(clientRepository));
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<ClientDTO>> GetClientsAsync()
+        {
+            var clientEntity = await _clientRepository.GetClientsAsync();
+            return _mapper.Map<IEnumerable<ClientDTO>>(clientEntity);
+        }
+
+        public async Task<ClientDTO> GetByIdAsync(Guid? id)
+        {
+            var clientEntity = await _clientRepository.GetByIdAsync(id);
+            return _mapper.Map<ClientDTO>(clientEntity);
+        }
+
+        public async Task<ClientDTO> GetByCPFAsync(CPF cpf)
+        {
+            var clientEntity = await _clientRepository.GetByCPFAsync(cpf);
+            return _mapper.Map<ClientDTO>(clientEntity);
+        }
+
+        public async Task<ClientDTO> GetByNameAsync(Name name)
+        {
+            var clientEntity = await _clientRepository.GetByNameAsync(name);
+            return _mapper.Map<ClientDTO>(clientEntity);
+        }
+
+        public async Task<ClientDTO> GetClientAccountAsync(Guid? id)
+        {
+            var clientEntity = await _clientRepository.GetClientAccountAsync(id);
+            return _mapper.Map<ClientDTO>(clientEntity);
+        }
+
+        public async Task AddAsync(ClientDTO clientDTO)
+        {
+            var clientEntity = _mapper.Map<Client>(clientDTO);
+            await _clientRepository.CreateAsync(clientEntity);
+        }
+
+        public async Task UpdateAsync(ClientDTO clientDTO)
+        {
+            var clientEntity = _mapper.Map<Client>(clientDTO);
+            await _clientRepository.UpdateAsync(clientEntity);
+        }
+
+        public async Task RemoveAsync(Guid? id)
+        {
+            var clientEntity = _clientRepository.GetByIdAsync(id).Result;
+            await _clientRepository.RemoveAsync(clientEntity);
+        }
+    }
+}
