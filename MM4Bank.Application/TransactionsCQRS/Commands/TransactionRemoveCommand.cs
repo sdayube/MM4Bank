e1a82d7f13845dba6cacfb0db50033ee5da00086
sdayube <scarige@gmail.com>
2021-11-14 20:08:09 -0300

    Add transaction commands

---
+++ MM4Bank.Application/TransactionsCQRS/Commands/TransactionRemoveCommand.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using MM4Bank.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MM4Bank.Application.Transactions.Commands
+{
+    public class TransactionRemoveCommand : IRequest<Transaction>
+    {
+        public Guid Id { get; set; }
+
+        public TransactionRemoveCommand(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
