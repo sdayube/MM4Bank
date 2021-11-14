2ea71c1bb55209f62aeedf781cca374d91fb0459
sdayube <scarige@gmail.com>
2021-11-14 20:08:36 -0300

    Add handlers

---
+++ MM4Bank.Application/TransactionsCQRS/Handlers/GetTransactionByIdQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using MM4Bank.Application.Transactions.Queries;
+using MM4Bank.Domain.Entities;
+using MM4Bank.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MM4Bank.Application.Transactions.Handlers
+{
+    public class GetTransactionByIdQueryHandler : IRequestHandler<GetTransactionByIdQuery, Transaction>
+    {
+        private readonly ITransactionRepository _transactionRepository;
+
+        public GetTransactionByIdQueryHandler(ITransactionRepository transactionRepository)
+        {
+            _transactionRepository = transactionRepository;
+        }
+
+        public async Task<Transaction> Handle(GetTransactionByIdQuery request, CancellationToken cancellationToken)
+        {
+            return await _transactionRepository.GetByIdAsync(request.Id);
+        }
+    }
+}
