b3b61653009d98b5bc5437c6e7786d95714421d7
sdayube <scarige@gmail.com>
2021-11-18 04:09:05 -0300

    Begin client controller

---
+++ MM4Bank.WebAPI/Controllers/ClientController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MM4Bank.Application.DTOs;
+using MM4Bank.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MM4Bank.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        private readonly IClientService _clientService;
+
+        public ClientController(IClientService clientService)
+        {
+            _clientService = clientService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ClientDTO>>> Get()
+        {
+            var accounts = await _clientService.GetClientsAsync();
+
+            if (accounts is null)
+            {
+                return NotFound("Accounts not found");
+            }
+
+            return Ok(accounts);
+        }
+    }
+}
